182e69e72cf824bb095c4edae9b7ec552c8e156b
Kapee87 <kapserker@gmail.com>
2022-05-02 15:29:39 -0300

    Primer subida a repo de encriptador challenge de Oracle-Alura Latam

---
+++ borrador.js
@@ -0,0 +1,103 @@
+
+// @import url('https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap');
+// :root{
+//     --main-color:#0A3871;
+//     --secondary-color:aliceblue;
+// }
+
+// *{
+//     box-sizing: border-box;
+//     margin: 0;
+//     padding: 0;
+    
+// }
+
+// body{
+//     margin: 1.8rem;
+//     display: flex;
+//     flex-direction: column;
+//     align-items: center;
+//     gap:2rem;
+//     font-family: 'Inter', sans-serif;
+// }
+
+// header{
+//     display: flex;
+//     align-self: baseline;
+    
+// }
+
+// .main{
+//     display: flex;
+//     flex-direction: column;
+    
+// }
+
+// .main .caja-encriptar #texto-encriptar, .caja-desencriptar #texto-desencriptar{
+//     width: 70vw;
+//     height: 30vh;
+//     border: none;
+//     font-weight: 400;
+//     font-size: 1.8rem;
+//     font-family: 'Inter', sans-serif;
+//     resize: none;
+    
+// }
+// .main #texto-encriptar ::placeholder{
+//     color: #0A3871;
+// }
+// .main .comentario{
+// display: flex;
+// align-items: center;
+// gap: 0.3rem;
+
+// }
+// .main .comentario svg{
+//     width: 1rem;
+// }
+// .main .comentario #comentario {
+// font-weight:normal;
+// font-size: 0.75rem;
+
+// }
+// .visible{
+//     display: initial;
+// }
+// .hidden{
+//     display: none;
+// }
+
+// .buttons-box{
+//     display: flex;
+//     flex-direction: column;
+//     gap: 1rem;
+// }
+// .buttons-box button{
+//     border: none;
+//     border-radius: 1.3rem;
+//     padding: 1.3rem;
+//     width: 20rem;
+//     font-weight: 400, regular;
+//     font-size: 1rem;
+//     line-height: 1.4rem;
+// }
+// .buttons-box #encriptar{
+//     background-color: var(--main-color);
+//     color:var(--secondary-color) ;
+// }
+// .buttons-box #desencriptar{
+//     background-color: white;
+//     color:var(--main-color) ;
+//     border: solid 1px var(--main-color);
+// }
+// .caja-desencriptar #vacio {
+// margin-top: 1rem;
+//     display: flex;
+//     flex-direction: column;
+//     align-items: center;
+//     flex-wrap: wrap;
+//     width: 50vw;
+//     text-align: center;
+//     gap: 1rem;
+
+// }
